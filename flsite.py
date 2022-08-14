59f6f9ff10a340247f59310d786831dcdaa611f0
Vlad <vladlapkovsky@gmail.com>
2022-08-14 19:32:59 +0300

    Add login and profile pages/templates

--- flsite.py
+++ flsite.py
@@ -1,4 +1,13 @@
-from flask import Flask, render_template, url_for, request, flash
+from flask import (
+    Flask,
+    render_template,
+    url_for,
+    request,
+    session,
+    flash,
+    redirect,
+    abort
+)
 from credentials import PROJECT_SECRET_KEY
 
 app = Flask(__name__)
@@ -8,7 +17,9 @@ app.config['SECRET_KEY'] = PROJECT_SECRET_KEY
 MENU = [
     {'name': 'Install', 'url': 'install-flask'},
     {'name': 'First app', 'url': 'first-app'},
-    {'name': 'Contact us', 'url': 'contact'}
+    {'name': 'Contact us', 'url': 'contact'},
+    {'name': 'About', 'url': 'about'},
+    {'name': 'Login', 'url': 'login'}
 ]
 
 
@@ -26,7 +37,10 @@ def about():
 
 @app.route('/profile/<username>')
 def profile(username):
-    return f"User: {username}"
+    if 'userLogged' not in session or session.get('userLogged') != username:
+        abort(401)
+    context = {'menu': MENU, 'title': 'Profile', 'username': username}
+    return render_template('profile.html', **context)
 
 
 @app.route('/contact', methods=['POST', 'GET'])
@@ -39,6 +53,18 @@ def contact():
 
     context = {'menu': MENU, 'title': 'Contact-us'}
     return render_template('contact.html', **context)
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if 'userLogged' in session:
+        return redirect(url_for('profile', username=session['userLogged']))
+    elif request.method == 'POST' and request.form.get('username') == 'vlad' and request.form.get('password') == '123':  # if user in DB
+        session['userLogged'] = request.form['username']
+        return redirect(url_for('profile', username=session['userLogged']))
+
+    context = {'menu': MENU, 'title': 'Login'}
+    return render_template('login.html', **context)
 
 
 @app.errorhandler(404)
