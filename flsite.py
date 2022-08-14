79644d224801b5a08c382a011bbc53cc98bd54b6
Vlad <vladlapkovsky@gmail.com>
2022-08-14 17:17:44 +0300

    Add profile view

--- flsite.py
+++ flsite.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
@@ -8,14 +8,25 @@ MENU = ['Install', 'First app', 'Contact us']
 @app.route('/')
 def index():
     context = {'menu': MENU, 'title': 'Home'}
+    print(f"{url_for('index')=}")
     return render_template('index.html', **context)
 
 
 @app.route('/about')
 def about():
     context = {'menu': MENU, 'title': 'About'}
+    print(f"{url_for('about')=}")
     return render_template('about.html', **context)
 
+
+@app.route('/profile/<username>')
+def profile(username):
+    return f"User: {username}"
+
+
+# with app.test_request_context():
+#     print(url_for('about'))
+#     print(url_for('profile', username='Vlad'))
 
 if __name__ == '__main__':
     app.run(debug=True)
