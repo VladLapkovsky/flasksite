ea4209e339c038865c7bfe075e8cd092b1051426
Vlad <vladlapkovsky@gmail.com>
2022-08-18 18:45:35 +0300

    Refactor FDataBase methods

--- FDataBase.py
+++ FDataBase.py
@@ -24,18 +24,15 @@ class FDataBase:
             sql_query = 'SELECT * FROM mainmenu'
         else:
             sql_query = f"""SELECT * FROM mainmenu WHERE NOT url LIKE '{url_for("logout")}'"""
-        try:
-            self.__cursor.execute(sql_query)
-            result = self.__cursor.fetchall()
-            if result:
-                return result
-        except Exception as e:
-            print('Error occurred while reading DB: ', e)
-        return []
+
+        execute_error, result = self._execute_sql_and_fetch_all(sql_query)
+
+        return result if result is not None else []
 
     def addPost(self, post_title, post_content, post_url):
         if self._is_post_url_exists(post_url):
-            return False
+            msg = f'Post with {post_url} URL already exists'
+            return False, msg
 
         base_path = url_for('static', filename='images_html')
         post_content = re.sub(
@@ -43,101 +40,186 @@ class FDataBase:
             "\\g<tag>" + base_path + "/\\g<url>>",
             post_content
         )
+
         sql_query = 'INSERT INTO posts VALUES(NULL, ?, ?, ?, ?)'
         post_adding_time = math.floor(time.time())
-        result = False
-        try:
-            self.__cursor.execute(sql_query, (post_title, post_content, post_url, post_adding_time))
-            self.__db.commit()
-        except sqlite3.Error as e:
-            print('sqlite3.Error occurred while adding post to DB: ', e)
-        except Exception as e:
-            print('Error occurred while adding post to DB: ', e)
+        is_added = False
+        msg = ''
+
+        execute_error, is_saved = self._execute_sql_and_save(sql_query, (
+            post_title, post_content, post_url, post_adding_time,
+        ))
+
+        if execute_error is None and is_saved is True:
+            is_added = True
         else:
-            result = True
-        return result
+            msg = f'Error occurred while adding post to DB: {execute_error}'
+        return is_added, msg
 
     def getPost(self, post_url):
         sql_query = f"SELECT title, text FROM posts WHERE url LIKE '{post_url}' LIMIT 1"
-        try:
-            self.__cursor.execute(sql_query)
-            result = self.__cursor.fetchone()
-            if result:
-                return result
-        except Exception as e:
-            print(f'Error occurred while getting post with URL {post_url} from DB: ', e)
-        return False, False
+
+        execute_error, result = self._execute_sql_and_fetch_one(sql_query)
+
+        return result
 
     def getPostsAnnounce(self):
         sql_query = 'SELECT title, text, url FROM posts ORDER BY time DESC'
-        try:
-            self.__cursor.execute(sql_query)
-            result = self.__cursor.fetchall()
-            if result:
-                return result
-        except Exception as e:
-            print('Error occurred while getting posts from DB: ', e)
-        return []
+
+        execute_error, result = self._execute_sql_and_fetch_all(sql_query)
+
+        return result if result is not None else []
 
     def addUser(self, username, email, hashed_password) -> typing.Tuple[bool, str]:
-        try:
-            if self._is_email_exists(email) is True:
-                return False, 'User with this email already exists'
-            self._add_user_to_db(username, email, hashed_password)
-        except SQLError as error:
-            return False, error.args[0]  # get Exception message
+        if self._is_email_exists(email) is True:
+            return False, 'User with this email already exists'
+
+        is_added = self._add_user_to_db(username, email, hashed_password)
+        if is_added is False:
+            return False, "User wasn't added, try again"  # get Exception message
 
         return True, 'Registration succeed'
 
-    def getUser(self, email, inputted_password) -> typing.Tuple[typing.Optional[sqlite3.Row], str]:
-        try:
-            if not self._is_email_exists(email):
-                return None, "User with email doesn't exist"
-            user = self._get_user_from_db(email)
+    # def getUserByID(self, user_id):
+    #     sql_query = f"SELECT * FROM users WHERE id = {user_id} LIMIT 1"
+    #     try:
+    #         if not self._is_email_exists(email):
+    #             return None, "User with email doesn't exist"
+    #         user = self._get_user_from_db(email)
+    #         if not check_password_hash(user['password'], inputted_password):
+    #             return None, "Wrong password"
+    #     except SQLError as error:
+    #         return None, error.args[0]  # get Exception message
+    #     else:
+    #         return user, ''
+
+        # try:
+        #     if not self._is_email_exists(email):
+        #         return None, "User with email doesn't exist"
+        #     user = self._get_user_from_db_by_email(email)
+        #     if not check_password_hash(user['password'], inputted_password):
+        #         return None, "Wrong password"
+        # except SQLError as error:
+        #     return None, error.args[0]  # get Exception message
+        # else:
+
+    def getUserFromLogin(self, email, inputted_password) -> typing.Tuple[typing.Optional[sqlite3.Row], str]:
+        if not self._is_email_exists(email):
+            return None, "User with email doesn't exist"
+
+        user = self._get_user_from_db_by_email(email)
+        if user is not None:
             if not check_password_hash(user['password'], inputted_password):
                 return None, "Wrong password"
-        except SQLError as error:
-            return None, error.args[0]  # get Exception message
+            else:
+                return user, ''
         else:
-            return user, ''
+            return None, f"There is no user related with {email}"
 
     def _is_email_exists(self, email):
         is_exists = False
-
         sql_query = f"SELECT COUNT() as `count` FROM users WHERE email LIKE '{email}'"
-        try:
-            self.__cursor.execute(sql_query)
-            sql_result = self.__cursor.fetchone()
-        except Exception as e:
-            raise SQLError('Error occurred while selecting email from DB') from e
-        else:
-            if sql_result['count'] > 0:
-                is_exists = True
+
+        self._execute_sql_query(sql_query)
+        sql_result = self._fetch_one_from_db()
+
+        if sql_result['count'] > 0:
+            is_exists = True
         return is_exists
 
-    def _add_user_to_db(self, username, email, hashed_password):
+    def _add_user_to_db(self, username, email, hashed_password) -> bool:
+        is_added = False
         sql_query = 'INSERT INTO users VALUES(NULL, ?, ?, ?, ?)'
         tm = math.floor(time.time())
-        try:
-            self.__cursor.execute(sql_query, (username, email, hashed_password, tm))
-            self.__db.commit()
-        except Exception as e:
-            raise SQLError('User adding error') from e
 
-    def _get_user_from_db(self, email):
+        execute_error, is_saved = self._execute_sql_and_save(sql_query, (username, email, hashed_password, tm))
+
+        if execute_error is None and is_saved is True:
+            is_added = True
+        return is_added
+
+    def _get_user_from_db_by_email(self, email):
         sql_query = f"SELECT * FROM users WHERE email LIKE '{email}' LIMIT 1"
-        try:
-            self.__cursor.execute(sql_query)
-            sql_result = self.__cursor.fetchone()
-        except Exception as e:
-            raise SQLError('User getting from DB error') from e
-        return sql_result
+
+        execute_error, result = self._execute_sql_and_fetch_one(sql_query)
+
+        return result
+
+    def _get_user_from_db_by_id(self, user_id):
+        sql_query = f"SELECT * FROM users WHERE id = {user_id} LIMIT 1"
+
+        execute_error, result = self._execute_sql_and_fetch_one(sql_query)
+
+        return result
 
     def _is_post_url_exists(self, post_url):
         sql_query = f"SELECT COUNT() as `count` FROM posts WHERE url LIKE '{post_url}'"
-        self.__cursor.execute(sql_query)
-        result = self.__cursor.fetchone()
-        if result['count'] > 0:
-            print(f'Post with {post_url} URL already exists')
+
+        execute_error, result = self._execute_sql_and_fetch_one(sql_query)
+
+        if execute_error is not None and result is not None and result['count'] > 0:
             return True
         return False
+
+    def _execute_sql_and_fetch_one(self, sql_query, *args):
+        result = None
+
+        execute_error = self._execute_sql_query(sql_query, *args)
+        if execute_error is None:
+            result = self._fetch_one_from_db()
+
+        return execute_error, result
+        # return self._fetch_one_from_db() if execute_error is None else execute_error
+
+    def _execute_sql_and_fetch_all(self, sql_query, *args):
+        result = None
+
+        execute_error = self._execute_sql_query(sql_query, *args)
+
+        if execute_error is None:
+            result = self._fetch_all_from_db()
+        return execute_error, result
+        # return self._fetch_all_from_db() if execute_error is None else execute_error
+
+    def _execute_sql_and_save(self, sql_query, *args):
+        is_saved = False
+
+        execute_error = self._execute_sql_query(sql_query, *args)
+
+        if execute_error is None:
+            is_saved = self._save_db()
+        return execute_error, is_saved
+
+    def _execute_sql_query(self, sql_query, *args):
+        execute_error = None
+        try:
+            self.__cursor.execute(sql_query, *args)
+        except Exception as e:
+            # log.error
+            execute_error = f'SQL query "{sql_query}" finished with error: {e}'
+        return execute_error
+
+    def _save_db(self):
+        is_saved = True
+        try:
+            self.__db.commit()
+        except Exception:
+            # log.error...
+            is_saved = False
+        return is_saved
+
+    def _fetch_one_from_db(self) -> typing.Optional[sqlite3.Row]:
+        return self.__fetch_from_db('fetchone')
+
+    def _fetch_all_from_db(self) -> typing.Optional[sqlite3.Row]:
+        return self.__fetch_from_db('fetchall')
+
+    def __fetch_from_db(self, fetch):
+        sql_result = None
+        try:
+            sql_result = getattr(self.__cursor, fetch)()
+        except Exception as e:
+            # log.error
+            # raise SQLError(f'"{fetch}()" from DB finished with error') from e
+            print(f'"{fetch}()" from DB finished with error', e)
+        return sql_result
