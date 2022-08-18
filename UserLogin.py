868c86c8726ac28bde2a191a2616f47f8c2b1ead
Vlad <vladlapkovsky@gmail.com>
2022-08-18 20:40:41 +0300

    Add UserMixin to remove is_authenticated, is_active, is_anonymous

--- UserLogin.py
+++ UserLogin.py
@@ -1,4 +1,7 @@
-class UserLogin:
+from flask_login import UserMixin
+
+
+class UserLogin(UserMixin):
     def fromDB(self, user_id, database):
         self.__user = database.getUserByID(user_id)
         return self
@@ -7,14 +10,14 @@ class UserLogin:
         self.__user = user
         return self
 
-    def is_authenticated(self):
-        return True
-
-    def is_active(self):
-        return True
-
-    def is_anonymous(self):
-        return False
+    # def is_authenticated(self):
+    #     return True
+    #
+    # def is_active(self):
+    #     return True
+    #
+    # def is_anonymous(self):
+    #     return False
 
     def get_id(self):
         return self.__user['id']
